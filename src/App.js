03fd5b35a195ef9ab000591ef624cf8edbc2c00c
rupesss-09 <rupesss09@gmail.com>
2021-12-08 21:11:50 -0600

    Some cleanup, added components and prevState practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+
+    const onClickHandler = () => {
+        // setCount(count + 1);
+        // setCount(count + 1);
+        setCount((prevState) => prevState + 1);
+        setCount((prevState) => prevState + 1);
+        console.log(count);
+    };
+
+    const onClickHandler1 = () => {
+        setCount((prevState) => prevState - 1);
+    };
+
+    return (
+        <div className='App'>
+            Hello World
+            <button onClick={onClickHandler}>+</button>
+            <button onClick={onClickHandler1}>-</button>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Content.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Content = () => {
+    return <div>This is a content !!!</div>;
+};
+
+export default Content;
