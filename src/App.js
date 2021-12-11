c81148a0be103e8c6f828ed991642da2e9ebcb34
rupesss-09 <rupesss09@gmail.com>
2021-12-10 19:19:17 -0600

    Adding AddItem field to add groceries list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from 'components/Header';
 import Content from 'components/Content';
 import Footer from 'components/Footer';
+import AddItem from 'components/AddItem';
 
 function App() {
     const [items, setItems] = useState([
@@ -23,26 +24,50 @@ function App() {
         },
     ]);
 
-    const onChangeHandler = (id) => {
+    const [newItem, setNewItem] = useState('');
+
+    const addItem = (item) => {
+        //Our items has id, checked and items properties.
+        // Items in array are objects that contains id, checked and items
+        const id = items.length ? items[items.length - 1].id + 1 : 1; // This is creating id for the object
+        const myNewItem = { id, checked: false, item };
+        const listItems = [...items, myNewItem];
+        setItems(listItems);
+    };
+
+    const onCheckHandler = (id) => {
         const listItems = items.map((item) =>
             item.id === id ? { ...item, checked: !item.checked } : item
         );
         setItems(listItems);
-        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+        //localStorage.setItem('shoppinglist', JSON.stringify(listItems));
     };
 
     const onDeleteHandler = (id) => {
         const listItems = items.filter((item) => item.id !== id);
         setItems(listItems);
-        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+        //localStorage.setItem('shoppinglist', JSON.stringify(listItems));
     };
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        if (!newItem) return;
+        addItem(newItem);
+    };
+
+    console.log(items);
 
     return (
         <div className='App'>
-            <Header />
+            <Header title={'Groceries List'} />
+            <AddItem
+                newItem={newItem}
+                setNewItem={setNewItem}
+                submitHandler={onSubmitHandler}
+            />
             <Content
                 items={items}
-                onChange={onChangeHandler}
+                onCheckHandler={onCheckHandler}
                 onDelete={onDeleteHandler}
             />
             <Footer length={items.length} />
