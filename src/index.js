1fde8c95183b4f096269700f553b1955db5d3fa6
rupesss-09 <rupesss09@gmail.com>
2021-12-26 13:57:32 -0600

    firebaseConfig from app setting from firebase console

--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const firebaseConfig = {
+    apiKey: 'AIzaSyDMDPAcqYy_9UYIOw9JkShghUoEkVReyYQ',
+    authDomain: 'grocery-list-9a69f.firebaseapp.com',
+    projectId: 'grocery-list-9a69f',
+    storageBucket: 'grocery-list-9a69f.appspot.com',
+    messagingSenderId: '849707778269',
+    appId: '1:849707778269:web:79236620bd9d496cb0db34',
+};
+
 ReactDOM.render(<App />, document.getElementById('root'));
