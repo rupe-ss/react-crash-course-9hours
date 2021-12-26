d3b6fca6e5405ed6901355fe5273e98fcca5b00f
rupesss-09 <rupesss09@gmail.com>
2021-12-26 14:37:58 -0600

    Moving firebaseConfig from index.js to dbconfig.js

---
+++ src/dbconfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyDMDPAcqYy_9UYIOw9JkShghUoEkVReyYQ',
+    authDomain: 'grocery-list-9a69f.firebaseapp.com',
+    projectId: 'grocery-list-9a69f',
+    storageBucket: 'grocery-list-9a69f.appspot.com',
+    messagingSenderId: '849707778269',
+    appId: '1:849707778269:web:79236620bd9d496cb0db34',
+};
+
+initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -3,17 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import { initializeApp } from 'firebase/app';
-
-const firebaseConfig = {
-    apiKey: 'AIzaSyDMDPAcqYy_9UYIOw9JkShghUoEkVReyYQ',
-    authDomain: 'grocery-list-9a69f.firebaseapp.com',
-    projectId: 'grocery-list-9a69f',
-    storageBucket: 'grocery-list-9a69f.appspot.com',
-    messagingSenderId: '849707778269',
-    appId: '1:849707778269:web:79236620bd9d496cb0db34',
-};
-
-initializeApp(firebaseConfig);
-
 ReactDOM.render(<App />, document.getElementById('root'));
