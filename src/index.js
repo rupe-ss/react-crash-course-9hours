2df664ec2f84ea530a4355db02da5113d3bcb8aa
rupesss-09 <rupesss09@gmail.com>
2021-12-26 14:07:24 -0600

    Adding initializeApp in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { initializeApp } from 'firebase/app';
+
 const firebaseConfig = {
     apiKey: 'AIzaSyDMDPAcqYy_9UYIOw9JkShghUoEkVReyYQ',
     authDomain: 'grocery-list-9a69f.firebaseapp.com',
@@ -11,5 +13,7 @@ const firebaseConfig = {
     messagingSenderId: '849707778269',
     appId: '1:849707778269:web:79236620bd9d496cb0db34',
 };
+
+initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
